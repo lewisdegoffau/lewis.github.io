14de4fc70cfe3dcbebbb33ee752c580002de9d83
lewisdegoffau <155606654+lewisdegoffau@users.noreply.github.com>
2024-01-04 17:16:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function animateBg(i) {
   document.body.style.backgroundColor = 'hsl(' + i + ', 100%, 50%)';
+  document.div.style.backgroundColor = 'hsl(' + i + ', 100%, 50%)';
   setTimeout(function() {
     animateBg(++i)
   }, i);
