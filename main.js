904af6ee24d698a3e6041e1a643d11df566b3bd5
lewisdegoffau <155606654+lewisdegoffau@users.noreply.github.com>
2024-01-04 17:08:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-function animateBg(i) {
-  document.body.style.backgroundColor = 'hsl(' + i + ', 100%, 50%)';
-  setTimeout(function() {
-    animateBg(++i)
-  }, i);
+var theme = document.querySelector('meta[name="theme-color"]'),
+  getThemeColor = theme.getAttribute('content'),
+  sectionElem = document.getElementsByTagName('section'),
+  sectionHeight = sectionElem[0].clientHeight,
+  sectionLength = sectionElem.length;
+
+// set color on load
+window.onload = () => {
+  document.body.style.backgroundColor = theme.getAttribute('content');
 }
-animateBg(0);
+
+// set color on 'scroll' event
+window.addEventListener('scroll', (e) => {
+  var offset = window.pageYOffset,
+    sectionIndex = parseInt(offset, "10") % sectionLength,
+    sectionColor = document.getElementsByTagName('section')[sectionIndex].getAttribute('data-theme-color'),
+    setSectionColor = theme.setAttribute('content', sectionColor);
+
+  document.querySelectorAll('section')[sectionIndex].style.backgroundColor = theme.getAttribute('content');
+});
